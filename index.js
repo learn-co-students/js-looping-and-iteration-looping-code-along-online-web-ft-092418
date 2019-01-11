347d4e6ba039c40a114b60030eee8576abd779dd
Peter-G-Stone <peter.stone.online@gmail.com>
2019-01-11 17:25:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(nameArr){
+    for (let i = 0; i < nameArr.length; i++){
+        console.log(`Welcome ${nameArr[i]}! You are employee #${i+1}.`)
+    }
+    return nameArr;
+}
+
+function maybeTrue(){
+    return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+    let i = 0;
+    while (maybeTrue()) {
+        i++
+    }
+    return `You got ${i} tails in a row!`;
+}
